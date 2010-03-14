7d47f564cd03fc17a0fd2fd4cf3079c6c8a0f393
tmb <tmb@hydra>
2010-03-14 10:55:31 -0700

    added page segmentation viewer

--- ocropy/components.py
+++ ocropy/components.py
@@ -5,36 +5,74 @@ import iulib,ocropus
 ### they will get hooks for pure Python implementations of these
 ### interfaces.
 
+component_registry = {}
+
+def register_constructor(name,f):
+    component_registry[name] = f
+
 def make_ICleanupGray(name):
+    if component_registry.has_key(name):
+        return component_registry[name]()
     return ocropus.make_ICleanupGray(name)
 def make_ICleanupBinary(name):
+    if component_registry.has_key(name):
+        return component_registry[name]()
     return ocropus.make_ICleanupBinary(name)
 def make_ITextImageClassification(name):
+    if component_registry.has_key(name):
+        return component_registry[name]()
     return ocropus.make_ITextImageClassification(name)
 def make_IRecognizeLine(name):
+    if component_registry.has_key(name):
+        return component_registry[name]()
     return ocropus.make_IRecognizeLine(name)
 def make_IBinarize(name):
+    if component_registry.has_key(name):
+        return component_registry[name]()
     return ocropus.make_IBinarize(name)
 def make_ISegmentPage(name):
+    if component_registry.has_key(name):
+        return component_registry[name]()
     return ocropus.make_ISegmentPage(name)
 def make_ISegmentLine(name):
+    if component_registry.has_key(name):
+        return component_registry[name]()
     return ocropus.make_ISegmentLine(name)
 def make_IExtractor(name):
+    if component_registry.has_key(name):
+        return component_registry[name]()
     return ocropus.make_IModel(name)
 def make_IModel(name):
+    if component_registry.has_key(name):
+        return component_registry[name]()
     return ocropus.make_IModel(name)
 def make_IComponent(name):
+    if component_registry.has_key(name):
+        return component_registry[name]()
     return ocropus.make_IComponent(name)
 def make_IDataset(name):
+    if component_registry.has_key(name):
+        return component_registry[name]()
     return ocropus.make_IDataset(name)
 def make_IExtDataset(name):
+    if component_registry.has_key(name):
+        return component_registry[name]()
     return ocropus.make_IExtDataset(name)
 def make_IFeatureMap(name):
+    if component_registry.has_key(name):
+        return component_registry[name]()
     return ocropus.make_IFeatureMap(name)
 def make_IGrouper(name):
+    if component_registry.has_key(name):
+        return component_registry[name]()
     return ocropus.make_IGrouper(name)
 def make_IDistComp(name):
+    if component_registry.has_key(name):
+        return component_registry[name]()
     return ocropus.make_IDistComp(name)
+
+### FIXME still need to figure out how to 
+### handle loading/saving Python components
 
 def load_ICleanupGray(file):
     return ocropus.load_ICleanupGray(file)
