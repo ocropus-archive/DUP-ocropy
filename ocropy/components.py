6808d7f9c9304e681cf3c44ee8612f2954070324
tmb <tmb@hydra>
2010-03-21 01:13:35 -0700

    updated ocropy to check interfaces

--- ocropy/components.py
+++ ocropy/components.py
@@ -25,96 +25,155 @@ def get_components(kind=None):
 
 def make_ICleanupGray(name):
     if component_registry.has_key(name):
-        return component_registry[name]()
-    return ocropus.make_ICleanupGray(name)
+        result = component_registry[name]()
+    else:
+        result = ocropus.make_ICleanupGray(name)
+    assert result.interface() == "ICleanupGray"
+    return result
 def make_ICleanupBinary(name):
     if component_registry.has_key(name):
-        return component_registry[name]()
-    return ocropus.make_ICleanupBinary(name)
+        result = component_registry[name]()
+    else:
+        result = ocropus.make_ICleanupBinary(name)
+    assert result.interface() == "ICleanupBinary"
+    return result
 def make_ITextImageClassification(name):
     if component_registry.has_key(name):
-        return component_registry[name]()
-    return ocropus.make_ITextImageClassification(name)
+        result = component_registry[name]()
+    else:
+        result = ocropus.make_ITextImageClassification(name)
+    assert result.interface() == "ITextImageClassification"
+    return result
 def make_IRecognizeLine(name):
     if component_registry.has_key(name):
-        return component_registry[name]()
-    return ocropus.make_IRecognizeLine(name)
+        result = component_registry[name]()
+    else:
+        result = ocropus.make_IRecognizeLine(name)
+    assert result.interface() == "IRecognizeLine"
+    return result
 def make_IBinarize(name):
     if component_registry.has_key(name):
-        return component_registry[name]()
-    return ocropus.make_IBinarize(name)
+        result = component_registry[name]()
+    else:
+        result = ocropus.make_IBinarize(name)
+    assert result.interface() == "IBinarize"
+    return result
 def make_ISegmentPage(name):
     if component_registry.has_key(name):
-        return component_registry[name]()
-    return ocropus.make_ISegmentPage(name)
+        result = component_registry[name]()
+    else:
+        result = ocropus.make_ISegmentPage(name)
+    assert result.interface() == "ISegmentPage"
+    return result
 def make_ISegmentLine(name):
     if component_registry.has_key(name):
-        return component_registry[name]()
-    return ocropus.make_ISegmentLine(name)
+        result = component_registry[name]()
+    else:
+        result = ocropus.make_ISegmentLine(name)
+    assert result.interface() == "ISegmentLine"
+    return result
 def make_IExtractor(name):
     if component_registry.has_key(name):
-        return component_registry[name]()
-    return ocropus.make_IModel(name)
+        result = component_registry[name]()
+    else:
+        result = ocropus.make_IExtractor(name)
+    assert result.interface() == "IExtractor"
+    return result
 def make_IModel(name):
     if component_registry.has_key(name):
-        return component_registry[name]()
-    return ocropus.make_IModel(name)
+        result = component_registry[name]()
+    else:
+        result = ocropus.make_IModel(name)
+    assert result.interface() == "IModel"
+    return result
 def make_IComponent(name):
     if component_registry.has_key(name):
-        return component_registry[name]()
-    return ocropus.make_IComponent(name)
+        result = component_registry[name]()
+    else:
+        result = ocropus.make_IComponent(name)
+    assert result.interface() == "IComponent"
+    return result
 def make_IDataset(name):
     if component_registry.has_key(name):
-        return component_registry[name]()
-    return ocropus.make_IDataset(name)
+        result = component_registry[name]()
+    else:
+        result = ocropus.make_IDataset(name)
+    assert result.interface() == "IDataset"
+    return result
 def make_IExtDataset(name):
     if component_registry.has_key(name):
-        return component_registry[name]()
-    return ocropus.make_IExtDataset(name)
+        result = component_registry[name]()
+    else:
+        result = ocropus.make_IExtDataset(name)
+    assert result.interface() == "IExtDataset"
+    return result
 def make_IFeatureMap(name):
     if component_registry.has_key(name):
-        return component_registry[name]()
-    return ocropus.make_IFeatureMap(name)
+        result = component_registry[name]()
+    else:
+        result = ocropus.make_IFeatureMap(name)
+    assert result.interface() == "IFeatureMap"
+    return result
 def make_IGrouper(name):
     if component_registry.has_key(name):
-        return component_registry[name]()
-    return ocropus.make_IGrouper(name)
+        result = component_registry[name]()
+    else:
+        result = ocropus.make_IGrouper(name)
+    assert result.interface() == "IGrouper"
+    return result
 def make_IDistComp(name):
     if component_registry.has_key(name):
-        return component_registry[name]()
-    return ocropus.make_IDistComp(name)
+        result = component_registry[name]()
+    else:
+        result = ocropus.make_IDistComp(name)
+    assert result.interface() == "IDistComp"
+    return result
 
 ### FIXME still need to figure out how to 
 ### handle loading/saving Python components
 
 def load_ICleanupGray(file):
-    return ocropus.load_ICleanupGray(file)
+    result = ocropus.load_ICleanupGray(file)
+    return result
 def load_ICleanupBinary(file):
-    return ocropus.load_ICleanupBinary(file)
+    result = ocropus.load_ICleanupBinary(file)
+    return result
 def load_ITextImageClassification(file):
-    return ocropus.load_ITextImageClassification(file)
+    result = ocropus.load_ITextImageClassification(file)
+    return result
 def load_IRecognizeLine(file):
-    return ocropus.load_IRecognizeLine(file)
+    result = ocropus.load_IRecognizeLine(file)
+    return result
 def load_IBinarize(file):
-    return ocropus.load_IBinarize(file)
+    result = ocropus.load_IBinarize(file)
+    return result
 def load_ISegmentPage(file):
-    return ocropus.load_ISegmentPage(file)
+    result = ocropus.load_ISegmentPage(file)
+    return result
 def load_ISegmentLine(file):
-    return ocropus.load_ISegmentLine(file)
+    result = ocropus.load_ISegmentLine(file)
+    return result
 def load_IExtractor(file):
-    return ocropus.load_IExtractor(file)
+    result = ocropus.load_IExtractor(file)
+    return result
 def load_IModel(file):
-    return ocropus.load_IModel(file)
+    result = ocropus.load_IModel(file)
+    return result
 def load_IComponent(file):
-    return ocropus.load_IComponent(file)
+    result = ocropus.load_IComponent(file)
+    return result
 def load_IDataset(file):
-    return ocropus.load_IDataset(file)
+    result = ocropus.load_IDataset(file)
+    return result
 def load_IFeatureMap(file):
-    return ocropus.load_IFeatureMap(file)
+    result = ocropus.load_IFeatureMap(file)
+    return result
 def load_IGrouper(file):
-    return ocropus.load_IGrouper(file)
+    result = ocropus.load_IGrouper(file)
+    return result
 def load_linerec(file):
-    return ocropus.load_linerec(file)
+    result = ocropus.load_linerec(file)
+    return result
 
 def save_component(file,component):
     ocropus.save_component(file,component)
