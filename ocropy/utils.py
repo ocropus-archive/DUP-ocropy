d5205fb1562d90fe22274edf34e7a9767fe2e5de
tmb <tmb@hydra>
2010-03-25 02:52:16 -0700

    tried another way of calling iulib::Tiff

--- ocropy/utils.py
+++ ocropy/utils.py
@@ -189,8 +189,10 @@ def page_iterator(files):
                 tiff = iulib.Tiff(file,"r")
                 for i in range(tiff.numPages()):
                     image = iulib.bytearray()
-                    tiff.getPageRaw(image,i,True)
-                    # tiff.getPage(image,i,True)
+                    try:
+                        tiff.getPageRaw(image,i,True)
+                    except:
+                        tiff.getPage(image,i,True)
                     yield image,"%s[%d]"%(file,i)
             else:
                 image = Image.open(file)
