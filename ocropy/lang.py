6f2cc1b61476e52f4d8b31abc154ed27c41c08ff
Tom <tmb@ixion.local>
2010-05-04 06:46:53 +0200

    merged release tags

--- ocropy/lang.py
+++ ocropy/lang.py
@@ -2,6 +2,17 @@ import sys,os,re,glob,math,glob,signal
 import iulib,ocropus
 from pylab import amax,zeros
 
+### character properties
+
+def size_category(c):
+    if len(c)>1: raise "isolated characters only"
+    if c in "acemnorsuvwxyz": return "x"
+    if c in "ABCDEFGHIJKLMNOPQRSTUVWXYZbdfhklt!?": return "k"
+    if c in "gpqy": return "y"
+    if c in ".,": return "."
+    if c in """'"`""": return "'"
+    return None
+
 ### commonly confused characters in OCR
 
 ocr_confusions_list = [
