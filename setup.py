554809efffb2bf234726389dd16a22fb4977cf31
Tom <tmb@ixion.local>
2012-07-31 22:11:07 +0200

    cleanup and checks

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
-"""
-setup.py file for SWIG example
-"""
+import sys
+try:
+    import scipy.ndimage
+    import tables
+except:
+    print
+    print "ERROR: import of scipy.ndimage or tables failed"
+    print
+    print "You may need to run ocropus-install-packages first."
+    print
+    sys.exit(1)
 
 import glob,os,os.path
 from distutils.core import setup, Extension
@@ -10,6 +18,15 @@ from distutils.command.install_data import install_data
 
 from ocrolib import default
 modelfiles = ["models/"+s for s in [default.model,default.space,default.ngraphs]]
+
+for fname in modelfiles:
+    if not os.path.exists(fname):
+        print
+        print "ERROR: cannot find",fname
+        print
+        print "You need to 'cd ./models' and run 'ocropus-download-models'"
+        print
+        sys.exit(1)
 
 setup (name = 'ocropy',
        version = '0.5',
