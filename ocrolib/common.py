57b90148f9a064297bb439b9d737c1ac15e16c2c
Tom <tmb@ixion.local>
2012-08-19 15:53:36 +0200

    moved path name defaults to default.py

--- ocrolib/common.py
+++ ocrolib/common.py
@@ -435,12 +435,7 @@ def summary(x):
 ### file name manipulation
 ################################################################
 
-def getlocal():
-    """Get the path to the local directory where OCRopus data is
-    installed. Checks OCROPUS_DATA in the environment first,
-    otherwise defaults to /usr/local/share/ocropus."""
-    local = os.getenv("OCROPUS_DATA") or "/usr/local/share/ocropus/"
-    return local
+from default import getlocal
 
 @checks(str,_=str)
 def findfile(name,error=1):
--- ocrolib/default.py
+++ ocrolib/default.py
@@ -21,3 +21,14 @@ installable += ["gradient.lineest"]
 
 installable += ["en-mixed-round1.lineest"]
 
+# these are system paths
+
+modeldir = "/usr/local/share/ocropus/"
+
+import os
+def getlocal():
+    """Get the path to the local directory where OCRopus data is
+    installed. Checks OCROPUS_DATA in the environment first,
+    otherwise defaults to /usr/local/share/ocropus."""
+    local = os.getenv("OCROPUS_DATA") or modeldir
+    return local
