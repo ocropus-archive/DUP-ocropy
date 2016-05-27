45800292de64f5e6a009399604e2761167bb3365
Emile Anclin <emile@cbien.com>
2016-05-27 14:56:38 +0200

    strongly improved network stripping

--- ocrolib/lstm.py
+++ ocrolib/lstm.py
@@ -292,6 +292,14 @@ class Softmax(Network):
         self.No = No
         self.W2 = randu(No,Nh+1)*initial_range
         self.DW2 = zeros((No,Nh+1))
+    def postLoad(self):
+        self.DW2 = zeros(self.W2.shape)
+
+    def preSave(self):
+        for var in ('state', 'DW2'):
+            if hasattr(self, var):
+                delattr(self, var)
+
     def ninputs(self):
         return self.Nh
     def noutputs(self):
@@ -596,6 +604,12 @@ class Stacked(Network):
         yield self
         for sub in self.nets:
             for x in sub.walk(): yield x
+    def preSave(self):
+        self.dstats = defaultdict(list)  # reset
+        for delta in ('deltas', 'ldeltas'):
+            if hasattr(self, delta):
+                delattr(self, delta)
+
     def ninputs(self):
         return self.nets[0].ninputs()
     def noutputs(self):
@@ -859,11 +873,16 @@ class SeqRecognizer:
         self.clear_log()
     def walk(self):
         for x in self.lstm.walk(): yield x
-    def clear_log(self):
+    def clear_log(self, deallocate_tempvars=False):
         self.command_log = []
         self.error_log = []
         self.cerror_log = []
         self.key_log = []
+        if deallocate_tempvars:
+            for attrname in ('outputs', 'targets', 'aligned'):
+                if hasattr(self, attrname):
+                    delattr(self, attrname)
+
     def __setstate__(self,state):
         self.__dict__.update(state)
         self.upgrade()
@@ -874,7 +893,7 @@ class SeqRecognizer:
         if "cerror_log" not in dir(self): self.cerror_log = []
         if "key_log" not in dir(self): self.key_log = []
     def info(self):
-        self.net.info()
+        self.lstm.info()
     def setLearningRate(self,r,momentum=0.9):
         self.lstm.setLearningRate(r,momentum)
     def predictSequence(self,xs):
