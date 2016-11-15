QuLogic/ocropy

py3k: Use print function instead of statement.

setup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import sys,time,urllib,traceback,glob,os,os.path
 
 assert sys.version_info[0]==2 and sys.version_info[1]>=7,\
@@ -9,13 +11,13 @@ from distutils.core import setup #, Extension, Command
 #from distutils.command.install_data import install_data
 
 if not os.path.exists("models/en-default.pyrnn.gz"):
-    print
-    print "You must download the default model 'en-default.pyrnn.gz'"
-    print "and put it into ./models."
-    print
-    print "Check https://github.com/tmbdev/ocropy for the location"
-    print "of model files."
-    print
+    print()
+    print("You must download the default model 'en-default.pyrnn.gz'")
+    print("and put it into ./models.")
+    print()
+    print("Check https://github.com/tmbdev/ocropy for the location")
+    print("of model files.")
+    print()
     sys.exit(1)
 
 scripts = [c for c in glob.glob("ocropus-*") if "." not in c and "~" not in c]

ocropus-archive/DUP-ocropy

setup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import sys,time,urllib,traceback,glob,os,os.path
 
 assert sys.version_info[0]==2 and sys.version_info[1]>=7,\
@@ -9,13 +11,13 @@ from distutils.core import setup #, Extension, Command
 #from distutils.command.install_data import install_data
 
 if not os.path.exists("models/en-default.pyrnn.gz"):
-    print
-    print "You should download the default model 'en-default.pyrnn.gz'"
-    print "and put it into ./models."
-    print
-    print "Check https://github.com/tmbdev/ocropy for the location"
-    print "of model files."
-    print
+    print()
+    print("You should download the default model 'en-default.pyrnn.gz'")
+    print("and put it into ./models.")
+    print()
+    print("Check https://github.com/tmbdev/ocropy for the location")
+    print("of model files.")
+    print()
 
 models = [c for c in glob.glob("models/*pyrnn.gz")]
 scripts = [c for c in glob.glob("ocropus-*") if "." not in c and "~" not in c]