ocropus-archive/DUP-ocropy

commit b4381aa7ab8133a57706252934354ad484c6cbc7

lstm.py: Make all imports explicit

ocrolib/lstm.py
@@ -25,11 +25,14 @@
 # License: Apache 2.0
 
 import common as ocrolib
-from pylab import *
+from numpy import (amax, amin, argmax, arange, array, clip, concatenate, dot,
+                   exp, isnan, log, maximum, mean, nan, ones, outer, roll, tanh,
+                   tile, vstack, zeros)
+from pylab import (clf, cm, figure, ginput, imshow, newaxis, rand, subplot,
+                   where)
 from collections import defaultdict
-from ocrolib.native import *
 from ocrolib.exceptions import RecognitionError
-from ocrolib import edist
+from ocrolib.edist import levenshtein
 import nutils
 import unicodedata
 from scipy.ndimage import measurements,filters
@@ -906,7 +909,7 @@ class SeqRecognizer:
         self.error = sum(deltas**2)
         self.error_log.append(self.error**.5/len(cs))
         # compute class error
-        self.cerror = edist.levenshtein(cs,result)
+        self.cerror = levenshtein(cs,result)
         self.cerror_log.append((self.cerror,len(cs)))
         # training keys
         self.key_log.append(key)

commit 2222227c76060b2f76188780fbb57168eb6efef2

Import sum from numpy as well in ocrolib/lstm.py
This fixes #178 which seems to be a regression from b4381aa

ocrolib/lstm.py
@@ -28,8 +28,8 @@ from __future__ import print_function
 
 import common as ocrolib
 from numpy import (amax, amin, argmax, arange, array, clip, concatenate, dot,
-                   exp, isnan, log, maximum, mean, nan, ones, outer, roll, tanh,
-                   tile, vstack, zeros)
+                   exp, isnan, log, maximum, mean, nan, ones, outer, roll, sum,
+                   tanh, tile, vstack, zeros)
 from pylab import (clf, cm, figure, ginput, imshow, newaxis, rand, subplot,
                    where)
 from collections import defaultdict